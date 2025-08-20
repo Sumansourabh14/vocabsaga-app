c02938be7a0a95b0aa06c577c15800fceb90c8ce
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-20 22:56:14 +0530

    added dark theme by default

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Slot } from "expo-router";
 
+const myTheme = {
+  ...DarkTheme,
+  colors: { ...DarkTheme.colors, primary: "#1b7a1b" },
+};
+
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <ThemeProvider value={myTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
 }
