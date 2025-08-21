8e7baca6631b3554d17236eec76f9023c44db9bc
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-22 00:22:28 +0530

    added meaning modal on story

--- app/story.tsx
+++ app/story.tsx
@@ -1,7 +1,7 @@
 import rawPassages from "@/data/passages/p1.json";
 import { WordPassage } from "@/types";
 import { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Modal, Pressable, StyleSheet, Text, View } from "react-native";
 
 const passages: WordPassage[] = rawPassages;
 
@@ -9,6 +9,7 @@ export default function Story() {
   const [current, setCurrent] = useState(
     Math.floor(Math.random() * passages.length)
   );
+  const [modalVisible, setModalVisible] = useState(false);
   const data = passages[current];
 
   const handleRandom = () => {
@@ -54,12 +55,77 @@ export default function Story() {
         justifyContent: "center",
         alignItems: "center",
       }}
-      className="px-8 bg-black"
+      className="px-8 bg-[#101010]"
     >
-      <Text className="text-center text-2xl text-white mb-8 font-playfair">
+      <Text className="text-center text-2xl text-gray-200 mb-8 font-playfair">
         {highlightWordInPassage(data.passages["15"], data.word)}
       </Text>
       <Button title="Shuffle" onPress={handleRandom} />
+      <Button title="Show Meaning" onPress={() => setModalVisible(true)} />
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text className="text-white mb-2 text-3xl font-playfairBold">
+              {data.word}
+            </Text>
+            <Text className="text-white mb-8 text-lg">{data.word_meaning}</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Done</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "#000",
+    borderRadius: 8,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 4,
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#FFFFFF",
+  },
+  textStyle: {
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
