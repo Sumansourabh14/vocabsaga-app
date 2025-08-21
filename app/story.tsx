6287a268438859b39cdcf1c35f0ac281449a87ec
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-21 22:40:29 +0530

    feat: added initial passage data

--- app/story.tsx
+++ app/story.tsx
@@ -1,6 +1,45 @@
+import rawPassages from "@/data/passages/p1.json";
+import { WordPassage } from "@/types";
+import { useState } from "react";
 import { Text, View } from "react-native";
 
+const passages: WordPassage[] = rawPassages;
+
 export default function Story() {
+  const [current, setCurrent] = useState(
+    Math.floor(Math.random() * passages.length)
+  );
+  const data = passages[current];
+
+  const highlightWordInPassage = (text: string, word: string) => {
+    const base = word.toLowerCase();
+    const forms = [
+      base,
+      `${base}s`,
+      `${base}es`,
+      `${base}d`,
+      `${base}ed`,
+      `${base}ing`,
+      base.endsWith("e") ? `${base.slice(0, -1)}ing` : "",
+      base.endsWith("e") ? `${base}d` : "",
+    ].filter((form): form is string => !!form); // Type guard to ensure string
+
+    const escapedForms = forms.map((w) =>
+      w.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+    );
+    const regex = new RegExp(`\\b(${escapedForms.join("|")})\\b`, "gi");
+
+    return text.split(regex).map((part, index) =>
+      index % 2 === 1 ? (
+        <Text key={index} className="text-lime-600 font-bold">
+          {part}
+        </Text>
+      ) : (
+        <Text key={index}>{part}</Text>
+      )
+    );
+  };
+
   return (
     <View
       style={{
@@ -8,8 +47,11 @@ export default function Story() {
         justifyContent: "center",
         alignItems: "center",
       }}
+      className="px-8"
     >
-      <Text>Vocabsaga</Text>
+      <Text className="text-center text-2xl">
+        {highlightWordInPassage(data.passages["15"], data.word)}
+      </Text>
     </View>
   );
 }
---
+++ types.ts
@@ -0,0 +1,11 @@
+export interface WordPassage {
+  id: string;
+  word: string;
+  word_meaning: string;
+  difficulty_level: string;
+  source_book?: string;
+  source_author?: string;
+  passages: {
+    [key: string]: string;
+  };
+}
