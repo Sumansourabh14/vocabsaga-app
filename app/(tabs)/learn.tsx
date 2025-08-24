47c5ebb5b60e0269421405005bb50d0b64246eac
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-24 13:06:56 +0530

    added accessibilityLabel attribute on learn button

--- app/(tabs)/learn.tsx
+++ app/(tabs)/learn.tsx
@@ -13,6 +13,8 @@ export default function Learn() {
       <View className="mt-6 px-6">
         <Pressable
           onPress={() => router.push("/story")}
+          accessibilityRole="button"
+          accessibilityLabel="Learn new words through passages"
           className="bg-white rounded-2xl shadow-md p-8 mb-4 flex-row items-center"
         >
           <Ionicons
@@ -23,10 +25,10 @@ export default function Learn() {
           />
           <View className="flex-1">
             <Text className="text-lg font-interBold mb-2">
-              Through passages
+              Learn through passages
             </Text>
             <Text className="text-gray-600 font-inter">
-              Learn new words in context by reading sentences.
+              Understand new words in context by reading sentences
             </Text>
           </View>
         </Pressable>
