039e8792ae71427d142dc47641c41f14a566ed64
Suman Sourabh <sumsourabh14@gmail.com>
2025-09-20 01:49:45 +0530

    added card background and subtitle color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,14 +3,18 @@ export const Colors = {
     background: "#191414",
     title: "#fff",
     text: "#fff",
+    subtitleText: "#f3f3f3",
     iconColor: "#fff",
     buttonBgActive: "#fff",
+    cardBackground: "#000",
   },
   light: {
     background: "#fff",
     title: "#000",
     text: "#000",
+    subtitleText: "gray",
     iconColor: "#000",
     buttonBgActive: "#000",
+    cardBackground: "#fff",
   },
 };
