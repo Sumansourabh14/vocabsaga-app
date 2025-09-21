ec9445a782092436c418b7cfa7fe879b52a216c4
Suman Sourabh <sumsourabh14@gmail.com>
2025-09-22 00:49:28 +0530

    updated onboarding theme for dark mode

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -2,19 +2,33 @@ import { useCustomTheme } from "@/context/CustomThemeContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Onboarding() {
   const router = useRouter();
-  const { theme } = useCustomTheme();
+  const colorScheme = useColorScheme();
+  const { theme, themeMode } = useCustomTheme();
+
+  const resolvedTheme =
+    themeMode === "system" ? (colorScheme ?? "light") : themeMode;
 
   return (
     <SafeAreaView className="flex-1 justify-center px-6">
       <View className="flex-1 items-center justify-center">
         <View className="items-center">
           <Image
-            source={require("@/assets/images/vocabsaga-full-light.png")}
+            source={
+              resolvedTheme === "dark"
+                ? require("@/assets/images/vocabsaga-full-dark.png")
+                : require("@/assets/images/vocabsaga-full-light.png")
+            }
             style={{ width: 300, height: 100, resizeMode: "contain" }}
           />
 
@@ -28,7 +42,7 @@ export default function Onboarding() {
       </View>
 
       <TouchableOpacity
-        className="px-10 py-4 rounded-xl mb-4"
+        className="px-10 py-4 rounded-lg mb-4"
         style={{ backgroundColor: theme.title }}
         onPress={async () => {
           await AsyncStorage.setItem("isFirstLaunch", "false");
