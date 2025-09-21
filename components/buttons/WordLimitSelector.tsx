4da6204d31ef03b24e60bdde21b118b0109d0c02
Suman Sourabh <sumsourabh14@gmail.com>
2025-09-22 00:55:57 +0530

    fixed incorrect colors on story for light mode

--- components/buttons/WordLimitSelector.tsx
+++ components/buttons/WordLimitSelector.tsx
@@ -1,5 +1,6 @@
 import { wordOptions } from "@/constants/WordOptions";
-import { Pressable, Text, useColorScheme, View } from "react-native";
+import { useCustomTheme } from "@/context/CustomThemeContext";
+import { Pressable, Text, View } from "react-native";
 
 type WordLimitSelectorProps = {
   wordLimit: string;
@@ -10,36 +11,31 @@ export default function WordLimitSelector({
   wordLimit,
   setWordLimit,
 }: WordLimitSelectorProps) {
-  const colorScheme = useColorScheme();
-  const isDark = colorScheme === "dark";
+  const { theme } = useCustomTheme();
 
   return (
     <View className="flex-row items-center gap-2 my-8">
       {wordOptions.map((opt) => {
+        const isSelected = wordLimit === opt.value;
+
         return (
           <Pressable
             key={opt.value}
             onPress={() => setWordLimit(opt.value)}
-            className={`px-6 py-2 rounded-lg border ${
-              wordLimit === opt.value
-                ? isDark
-                  ? "bg-white"
-                  : "bg-black"
-                : isDark
-                  ? "border-gray-600"
-                  : "border-gray-300"
-            }`}
+            className="px-6 py-2 rounded-lg border"
+            style={{
+              backgroundColor: isSelected ? theme.text : theme.cardBackground,
+              borderColor: isSelected ? theme.text : theme.borderColor,
+            }}
           >
             <Text
-              className={`text-sm ${
-                wordLimit === opt.value
-                  ? isDark
-                    ? "text-black font-interBold"
-                    : "text-white font-interBold"
-                  : isDark
-                    ? "text-gray-200 font-inter"
-                    : "text-gray-700 font-inter"
-              }`}
+              className="text-sm"
+              style={{
+                color: isSelected ? theme.cardBackground : theme.text,
+                fontFamily: isSelected
+                  ? "Inter_18pt-Bold"
+                  : "Inter_18pt-Regular",
+              }}
             >
               {opt.label}
             </Text>
