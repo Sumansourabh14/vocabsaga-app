2ac6b3c1acbe931cae81a351e02275fb42473f2f
Suman Sourabh <sumsourabh14@gmail.com>
2025-09-25 01:10:47 +0530

    added displays version from app.json file test on about

--- __tests__/About.test.tsx
+++ __tests__/About.test.tsx
@@ -1,6 +1,10 @@
 import About from "@/app/(tabs)/about";
 import { render } from "@testing-library/react-native";
 
+jest.mock("@/app.json", () => ({
+  expo: { version: "__TEST-VERSION__" },
+}));
+
 describe("About", () => {
   it("displays title and description without crashing", () => {
     const { getByText } = render(<About />);
@@ -18,5 +22,12 @@ describe("About", () => {
 
     expect(getByText("Privacy Policy")).toBeOnTheScreen();
     expect(getByText("Terms of Use")).toBeOnTheScreen();
+  });
+
+  it("displays version from app.json file", () => {
+    const { getByText } = render(<About />);
+
+    const versionText = getByText(`Version __TEST-VERSION__`);
+    expect(versionText).toBeOnTheScreen();
   });
 });
